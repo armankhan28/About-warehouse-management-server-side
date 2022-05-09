db3369f55352e49ae1bcce9104a2363e1d98c1c2
captainkhan28 <86075671+captainkhan28@users.noreply.github.com>
2022-05-09 13:54:39 +0600

    commit

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const port = process.env.PORT || 5000;
+
 const app = express();
 
 //middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wf9da.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log('CarDealer');
+  // perform actions on the collection object
+  client.close();
+});
+
+
+app.get('/', (req, res) => {
+    res.send('running server');
+});
+
+app.listen(port, () => {
+    console.log('listening',port);
+})
