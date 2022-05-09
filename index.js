a5f781f7e7edb65b440d29100fc9468bf826fe42
captainkhan28 <86075671+captainkhan28@users.noreply.github.com>
2022-05-09 15:36:47 +0600

    commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -11,16 +12,27 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wf9da.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('CarDealer');
-  // perform actions on the collection object
-  client.close();
-});
 
+async function run (){
+    try{
+        await client.connect();
+        const serviceCollection = client.db('carDealer').collection('service');
+
+        app.get('/service', async(req, res)=> {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+    }
+    finally{}
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('running server');
