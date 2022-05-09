d202fc0f67411c37917b694a2b93788a0bf93b2c
captainkhan28 <86075671+captainkhan28@users.noreply.github.com>
2022-05-09 20:18:31 +0600

    commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -26,6 +26,28 @@ async function run (){
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
+        });
+
+        app.get('/service/:id', async (req, res) =>{
+            const id = req.params.id;
+            const query ={_id: ObjectId(id)};
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        });
+
+        //POST
+        app.post('/service', async(req, res) => {
+            const newService = req.body;
+            const result = await serviceCollection.insertOne(newService);
+            res.send(result);
+        });
+
+        //Delete
+        app.delete('/service/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await serviceCollection.deleteOne(query);
+            res.send(result);
         });
 
     }
